7e0b5b79da4858c109db49f0cf92f6ef54d1b23f
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-07 15:43:57 +0900

    completa drag and drop first lesson

--- draganddrop/src/App.js
+++ draganddrop/src/App.js
@@ -1,10 +1,28 @@
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <DragDropContext></DragDropContext>
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {(provited) => (
+            <div {...provited.droppableProps} ref={provited.innerRef}>
+              <Draggable draggableId="item0" index={0}>
+                {(provited) => (
+                  <div
+                    ref={provited.innerRef}
+                    {...provited.draggableProps}
+                    {...provited.dragHandleProps}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   );
 }
