0fd9af89699d2d98d2234db5cf801cac3a0fbcf9
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-07 14:54:02 +0900

    add map funciton lecture

--- basicjsrestudy/src/App.js
+++ basicjsrestudy/src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
 
 function App() {
+  // map function
+  const list = [1, 2, 3, 4];
+  const dataList = [
+    {
+      name: "taro",
+      age: 21,
+    },
+    {
+      name: "suzuki",
+      age: 19,
+    },
+  ];
+
   return (
     <div className="App">
-      <h1>sample code</h1>
+      <h1>Map lesson</h1>
+      {list.map((number) => (
+        <div>{number * 2}</div>
+      ))}
+      {dataList.map((data) => (
+        <>
+          <div>{data.name}</div>
+          <div>{data.age}</div>
+        </>
+      ))}
     </div>
   );
 }
