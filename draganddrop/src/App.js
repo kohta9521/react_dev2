fd8c2969f4996c82d38b4d0d32af9d4699d59feb
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-07 16:04:51 +0900

    add all fiels but error about drag and drop hairetu not move

--- draganddrop/src/App.js
+++ draganddrop/src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
 
 function App() {
+  const [items] = useState(["item0", "item1", "item2"]);
+
+  const onDragEnd = (result) => {
+    const remove = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provited) => (
             <div {...provited.droppableProps} ref={provited.innerRef}>
@@ -16,10 +23,35 @@ function App() {
                     {...provited.draggableProps}
                     {...provited.dragHandleProps}
                   >
-                    item0
+                    {items[0]}
                   </div>
                 )}
               </Draggable>
+              <Draggable draggableId="item1" index={0}>
+                {(provited) => (
+                  <div
+                    className="item"
+                    ref={provited.innerRef}
+                    {...provited.draggableProps}
+                    {...provited.dragHandleProps}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item2" index={0}>
+                {(provited) => (
+                  <div
+                    className="item"
+                    ref={provited.innerRef}
+                    {...provited.draggableProps}
+                    {...provited.dragHandleProps}
+                  >
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {provited.placeholder}
             </div>
           )}
         </Droppable>
