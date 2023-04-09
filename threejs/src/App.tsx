41c5086c1e20dd6b9e78d75274f75fd4a92ae86e
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-09 15:03:51 +0900

    add complete about threejs box

--- threejs/src/App.tsx
+++ threejs/src/App.tsx
@@ -1,17 +1,21 @@
-import './App.css'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Box } from '@react-three/drei';
 
 function App() {
 
   return (
     <div className="w-full h-screen">
       <Canvas> 
-        <ambientLight intensity={0.1} />
-        <directionalLight color="white" position={[0, 8, 5]} />
+        {/* <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 8, 5]} /> */}
         <mesh>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial color="green" />
         </mesh>
+        <Box>
+          <meshStandardMaterial color="red" />
+        </Box>
+        <OrbitControls enableZoom={false} />
       </Canvas>
     </div>
   )
