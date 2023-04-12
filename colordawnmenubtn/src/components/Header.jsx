0fe103893056cd09fb81896ae47447740a2c38bb
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-12 15:32:26 +0900

    add js code in react project

--- colordawnmenubtn/src/components/Header.jsx
+++ colordawnmenubtn/src/components/Header.jsx
@@ -4,7 +4,6 @@ export const Header = () => {
   return (
     <div className="header">
       <div className="headerContainer">
-        <h1>Header</h1>
         <Hamber />
       </div>
     </div>
