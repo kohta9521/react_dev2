f99e1e30c8b6776c7557f46682e3f6686b74b01f
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-12 16:00:36 +0900

    add react responsive and styled-componetns

--- colordawnmenubtn/src/components/Header.jsx
+++ colordawnmenubtn/src/components/Header.jsx
@@ -7,13 +7,41 @@ export const Header = () => {
         <div className="App">
             <MediaQuery query="(max-width: 767px)">
                 <MainContainerPhone>
-                  <h1>PHONE Header</h1>              
+                  <div className="header">
+                    <div className="logo">
+                      <h2 className='logo-item'>LOGO</h2>
+                    </div>
+                    <div className="menu">
+                      <ul className='menu-list'>
+                        <li className='menu-item'><a href="">HOME</a></li>
+                        <li className='menu-item'><a href="">ABOUT</a></li>
+                        <li className='menu-item'><a href="">SERVICE</a></li>
+                        <li className='menu-item'><a href="">MEMBER</a></li>
+                        <li className='menu-item'><a href="">FAQ</a></li>
+                        <li className='menu-item'><a href="">CONTACT</a></li>
+                      </ul>
+                    </div>
+                  </div>
                 </MainContainerPhone>
             </MediaQuery>
 
             <MediaQuery query="(min-width: 768px)">
                 <MainContainerPC>
-                  <h1>PC Header</h1>        
+                <div className="header">
+                    <div className="logo">
+                      <h2 className='logo-item'>LOGO</h2>
+                    </div>
+                    <div className="menu">
+                      <ul className='menu-list'>
+                        <li className='menu-item'><a href="">HOME</a></li>
+                        <li className='menu-item'><a href="">ABOUT</a></li>
+                        <li className='menu-item'><a href="">SERVICE</a></li>
+                        <li className='menu-item'><a href="">MEMBER</a></li>
+                        <li className='menu-item'><a href="">FAQ</a></li>
+                        <li className='menu-item'><a href="">CONTACT</a></li>
+                      </ul>
+                    </div>
+                  </div>     
                 </MainContainerPC>
             </MediaQuery>
         </div>
@@ -22,16 +50,12 @@ export const Header = () => {
 
 const MainContainerPhone = styled.div`
     width: 100%;
-    > img {
-        width: 100%;
-    }
+    height: 100px;
 `;
 
 const MainContainerPC = styled.div`
     max-width: 800px;
     width: 100%;
     margin: 0 auto;
-    > img {
-        width: 100%;
-    }
+    height: 100px;
 `;
