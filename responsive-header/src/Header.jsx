c1ea6e82edb4f219878ceb5bb649df05220896de
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-18 10:22:37 +0900

    add all files and npm install about material ui and react reouter dom

---
+++ responsive-header/src/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+export default function Header() {
+  const [ state, setState ] = useState({
+    mobileView: false,
+  });
+
+  const { mobileView } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+      ? setState((prevState) => ({ ...prevState, mobileView: true }))
+      :  setState((prevState) => ({ ...prevState, mobileView: false}));
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    }
+  })
+  
+}
