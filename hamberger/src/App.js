f26d909079f61605f28d86a497e0f9fbf6808dc3
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-19 11:07:04 +0900

    add nasic all section components

--- hamberger/src/App.js
+++ hamberger/src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Projects from "./Projects";
 
 function App() {
   return (
-    <div className="App">
-      <h2>sample code</h2>
+    <div>
+      <Header />
+      <Home />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
